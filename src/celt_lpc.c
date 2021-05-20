f8c6e4eaa14eb3c9dd69b219301bc53443a1b11c
Roman Gaiu <roman.gaiu@ringcentral.com>
2021-05-19 17:05:54 -0700

    Replace dynamic size arrays with dynamically allocated buffers

--- src/celt_lpc.c
+++ src/celt_lpc.c
@@ -96,7 +96,7 @@ void celt_fir(
          int ord)
 {
    int i,j;
-   opus_val16 rnum[ord];
+   opus_val16 *rnum = (opus_val16 *)malloc(ord * sizeof(opus_val16));
    for(i=0;i<ord;i++)
       rnum[i] = num[ord-i-1];
    for (i=0;i<N-3;i+=4)
@@ -119,6 +119,8 @@ void celt_fir(
          sum = MAC16_16(sum,rnum[j],x[i+j-ord]);
       y[i] = ROUND16(sum, SIG_SHIFT);
    }
+
+   free(rnum);
 }
 
 void celt_iir(const opus_val32 *_x,
@@ -147,8 +149,8 @@ void celt_iir(const opus_val32 *_x,
 #else
    int i,j;
    celt_assert((ord&3)==0);
-   opus_val16 rden[ord];
-   opus_val16 y[N+ord];
+   opus_val16 *rden = (opus_val16*)malloc(ord * sizeof(opus_val16));
+   opus_val16 *y = (opus_val16*)malloc((N + ord) * sizeof(opus_val16));
    for(i=0;i<ord;i++)
       rden[i] = den[ord-i-1];
    for(i=0;i<ord;i++)
@@ -192,6 +194,9 @@ void celt_iir(const opus_val32 *_x,
    }
    for(i=0;i<ord;i++)
       mem[i] = _y[N-i-1];
+
+   free(rden);
+   free(y);
 #endif
 }
 
@@ -208,7 +213,7 @@ int _celt_autocorr(
    int fastN=n-lag;
    int shift;
    const opus_val16 *xptr;
-   opus_val16 xx[n];
+   opus_val16 *xx = (opus_val16*)malloc(n * sizeof(opus_val16));
    celt_assert(n>0);
    celt_assert(overlap>=0);
    if (overlap == 0)
@@ -275,5 +280,6 @@ int _celt_autocorr(
    }
 #endif
 
+   free(xx);
    return shift;
 }
--- src/pitch.c
+++ src/pitch.c
@@ -297,9 +297,9 @@ void pitch_search(const opus_val16 *x_lp, opus_val16 *y,
    celt_assert(max_pitch>0);
    lag = len+max_pitch;
 
-   opus_val16 x_lp4[len>>2];
-   opus_val16 y_lp4[lag>>2];
-   opus_val32 xcorr[max_pitch>>1];
+   opus_val16* x_lp4 = (opus_val16*)malloc((len >> 2) * sizeof(opus_val16));
+   opus_val16* y_lp4 = (opus_val16*)malloc((lag >> 2) * sizeof(opus_val16));
+   opus_val32* xcorr = (opus_val32*)malloc((max_pitch >> 1) * sizeof(opus_val32));
 
    /* Downsample by 2 again */
    for (j=0;j<len>>2;j++)
@@ -382,6 +382,10 @@ void pitch_search(const opus_val16 *x_lp, opus_val16 *y,
       offset = 0;
    }
    *pitch = 2*best_pitch[0]-offset;
+
+   free(x_lp4);
+   free(y_lp4);
+   free(xcorr);
 }
 
 #ifdef FIXED_POINT
@@ -443,7 +447,7 @@ opus_val16 remove_doubling(opus_val16 *x, int maxperiod, int minperiod,
       *T0_=maxperiod-1;
 
    T = T0 = *T0_;
-   opus_val32 yy_lookup[maxperiod+1];
+   opus_val32* yy_lookup = (opus_val32*)malloc((maxperiod + 1) * sizeof(opus_val32));
    dual_inner_prod(x, x, x-T0, N, &xx, &xy);
    yy_lookup[0] = xx;
    yy=xx;
@@ -522,5 +526,7 @@ opus_val16 remove_doubling(opus_val16 *x, int maxperiod, int minperiod,
 
    if (*T0_<minperiod0)
       *T0_=minperiod0;
+
+   free(yy_lookup);
    return pg;
 }
